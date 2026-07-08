0f712028e492063cff4af0512da94e92dd6986dc
Rhayonne <rhayonne@gmail.com>
2026-07-08 23:50:46 +0200

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base:'/meteo-basse-normandie/',
   build: {
     outDir: 'dist',
-    base:'/meteo-basse-normandie/'
   },
 });
